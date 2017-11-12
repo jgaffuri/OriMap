2498b19f40fac04708d17a3d2311cadd899f9f3e
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-12 20:33:45 +0100

    a

--- src/main/java/org/orimap/OriSHP.java
+++ src/main/java/org/orimap/OriSHP.java
@@ -4,21 +4,37 @@
 package org.orimap;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 /**
  * @author julien Gaffuri
  *
  */
 public class OriSHP {
-
+	private static final String specsFile = "";
 
 	//create empty shp files for orienteering map template
 	public static void createSHPRepository(String path) {
 		//ensure folders are there
 		new File(path).mkdirs();
 
-		
-		
+		try {
+			Iterable<CSVRecord> entries = CSVFormat.EXCEL.parse(new FileReader(specsFile));
+			for (CSVRecord entry : entries) {
+				//String lastName = record.get("Last Name");
+				//String firstName = record.get("First Name");
+			}
+		}
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+		catch (IOException e) { e.printStackTrace(); }
+
+
 	}
 
 	public static void main(String[] args) {
