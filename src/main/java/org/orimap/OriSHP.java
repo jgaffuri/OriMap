e75b3b816afe8b35d3a99a211e2fdd0480b42342
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-12 20:39:26 +0100

    a

--- src/main/java/org/orimap/OriSHP.java
+++ src/main/java/org/orimap/OriSHP.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
@@ -17,7 +16,7 @@ import org.apache.commons.csv.CSVRecord;
  *
  */
 public class OriSHP {
-	private static final String specsFile = "";
+	private static final String specsFile = "src/main/resources/specs.csv";
 
 	//create empty shp files for orienteering map template
 	public static void createSHPRepository(String path) {
@@ -25,10 +24,10 @@ public class OriSHP {
 		new File(path).mkdirs();
 
 		try {
-			Iterable<CSVRecord> entries = CSVFormat.EXCEL.parse(new FileReader(specsFile));
-			for (CSVRecord entry : entries) {
-				//String lastName = record.get("Last Name");
-				//String firstName = record.get("First Name");
+			Iterable<CSVRecord> entries = CSVFormat.RFC4180.withHeader("code", "geom", "desc").parse(new FileReader(specsFile));
+			for (CSVRecord e : entries) {
+				System.out.println(e.get("code") + " - " + e.get("geom") + " - " + e.get("desc"));
+				//TODO create SHP
 			}
 		}
 		catch (FileNotFoundException e) { e.printStackTrace(); }
@@ -38,7 +37,9 @@ public class OriSHP {
 	}
 
 	public static void main(String[] args) {
-		createSHPRepository("/home/juju/Bureau/orienteering/omap_luxembourg_shp/shp_/");
+		System.out.println("Start");
+		createSHPRepository("/home/juju/Bureau/orienteering/omap_luxembourg_shp/shpDev/");
+		System.out.println("end");
 	}
 
 }
