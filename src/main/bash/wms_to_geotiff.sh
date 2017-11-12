67c044e30593bb954411e06fe86bd93e205103b7
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-12 20:20:06 +0100

    a

---
+++ src/main/bash/wms_to_geotiff.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#resolution=10cm - max image size is 2500px -> 250m*250m
+
+#outdir="lux_city"
+#xmin_=76750
+#ymin_=74250
+#nbx=5
+#nby=6
+
+#outdir="img_kirchberg_hospital_park"
+#xmin_=80500
+#ymin_=77000
+#nbx=3
+#nby=4
+
+#outdir="img_coque"
+#xmin_=78500
+#ymin_=76250
+#nbx=4
+#nby=4
+
+#other part
+#outdir="img_arboretum"
+#xmin_=79250
+#ymin_=76750
+#nbx=3
+#nby=4
+
+#outdir="img_schetterhaard"
+#xmin_=85000
+#ymin_=75500
+#nbx=7
+#nby=6
+
+#outdir="img_grengewald"
+#xmin_=78000
+#ymin_=79000
+#nbx=10
+#nby=10
+
+outdir="img_grengewald_niederanven"
+xmin_=81500
+ymin_=78750
+nbx=17
+nby=13
+
+#outdir="img_parc_neuman"
+#xmin_=75250
+#ymin_=75500
+#nbx=5
+#nby=4
+
+#senningerberg
+#outdir="senningerberg"
+#xmin_=83950
+#ymin_=79000
+#nbx=1
+#nby=1
+
+
+crs=EPSG:2169
+
+mkdir $outdir
+
+#for layer in ortho_2016 topo_5k cadastre ortho_irc
+for layer in ortho_irc
+do
+
+mkdir $outdir/$layer
+
+for (( x=0; x<$nbx; x++ ))
+do
+	for (( y=0; y<$nby; y++ ))
+	do
+		xmin=$(($xmin_+$x*250))
+		ymin=$(($ymin_+$y*250))
+		xmax=$(($xmin+250))
+		ymax=$(($ymin+250))
+		name=$layer"_"$xmin"_"$ymin
+		echo $name
+
+		url="http://wmts1.geoportail.lu/opendata/service?REQUEST=GetMap&version=1.1.1&layers=$layer&srs=$crs&format=image/png&bbox=$xmin,$ymin,$xmax,$ymax&width=2500&height=2500&styles="
+		curl -o $outdir/$layer/$name.png $url 
+		gdal_translate -a_srs ${crs} -a_ullr $xmin $ymax $xmax $ymin $outdir/$layer/$name.png $outdir/$layer/$name.tif
+		rm $outdir/$layer/$name.png
+	done
+done
+
+done
