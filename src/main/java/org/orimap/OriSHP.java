5c0c6936a17f80d3972934d2141845b573d034a3
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-12 20:23:39 +0100

    a

---
+++ src/main/java/org/orimap/OriSHP.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.orimap;
+
+import java.io.File;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class OriSHP {
+
+	public static void createSHPRepository(String path) {
+		//ensure folders are there
+		new File(path).mkdirs();
+	}
+
+	public static void main(String[] args) {
+		createSHPRepository("/home/juju/Bureau/orienteering/omap_luxembourg_shp/shp_/");
+	}
+
+}
