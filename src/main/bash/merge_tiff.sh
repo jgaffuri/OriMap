e3b14b0d5d024d06f58f8f2b711f00b7b4a54081
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-07 14:44:22 +0100

    a

--- src/main/bash/merge_tiff.sh
+++ src/main/bash/merge_tiff.sh
@@ -1,5 +1,6 @@
 
 #QGIS: raster > miscaleanous > merge
+# + save as
 
 #gdal_merge.py -of HFA -o /home/juju/orienteering/omap_coque/img_arboretum/ortho_2017_arbor_.tif /home/juju/orienteering/omap_coque/img_arboretum/ortho_latest/ortho_latest_79250_76750.tif /home/juju/orienteering/omap_coque/img_arboretum/ortho_latest/ortho_latest_79250_77000.tif /home/juju/orienteering/omap_coque/img_arboretum/ortho_latest/ortho_latest_79250_77250.tif /home/juju/orienteering/omap_coque/img_arboretum/ortho_latest/ortho_latest_79250_77500.tif /home/juju/orienteering/omap_coque/img_arboretum/ortho_latest/ortho_latest_79500_76750.tif
 
