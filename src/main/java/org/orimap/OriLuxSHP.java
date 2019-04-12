973ef0b96d680c1507bc5c9ebef1760e647a4fc2
jgaffuri <jgaf@protonmail.com>
2019-04-12 18:48:35 +0200

    a

--- src/main/java/org/orimap/OriLuxSHP.java
+++ src/main/java/org/orimap/OriLuxSHP.java
@@ -12,7 +12,7 @@ import org.opencarto.util.FeatureUtil;
  * @author julien Gaffuri
  *
  */
-public class OriSHP {
+public class OriLuxSHP {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
@@ -22,15 +22,31 @@ public class OriSHP {
 		String outBasePath = "/home/juju/Bureau/out/";
 		double equidistance = 10;
 
-
-
+		/*
+		 *ADM
+		 *ALTI
+		 *BATI
+		 *GEO
+		HYDR
+		 *NYM
+		 *ORO
+		VCR
+		VEGE
+		VFTE
+		 */
 
 
 		//101,L,contour
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ALTI/COURBE.shp NATURE=0
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ALTI/COURBE.shp NATURE=3
 		//102,L,index_contour
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ALTI/COURBE.shp NATURE=1
 		//103,L,form_line
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ALTI/COURBE.shp NATURE=2
 		//104,L,earth_bank
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ORO/LIGNE_ORO.shp NATURE=0 2
 		//105,L,earth_wall
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ORO/LIGNE_ORO.shp NATURE=1
 		//106,L,ruined_earth_wall
 		//107,L,erosion_gully
 		//108,L,small_erosion_gully
@@ -70,29 +86,44 @@ public class OriSHP {
 		//311,P,well_fountain_water_tank
 		//312,P,spring
 		//313,P,prominent_water_feature
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_PONCT.shp NATURE= 12
 
 		//401,S,open_land
 		//402,S,open_land_scattered_trees
+		//402.1,S,open_land_scattered_trees
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_SURF.shp NATURE=5
 		//403,S,rough_open_land
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/TERR_SPORT.shp
 		//404,S,rough_open_land_scattered_trees
 		//405,S,forest
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_SURF.shp NATURE=0 1 2
 		//406,S,Vegetation_slow
 		//407,S,vegetation_slow_good_visibility
 		//408,S,vegetation_walk
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_SURF.shp NATURE=3
 		//409,S,vegetation_walk_good_visibility
 		//410,S,vegetation_fight
 		//411,S,vegetation_impassable
+		//411.2,L,vegetation_impassable_minw
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_LIN.shp NATURE=1 2
 		//412,S,cultivated_land
 		//413,S,orchard
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_SURF.shp NATURE=4
 		//414,S,vineyard_similar
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_SURF.shp NATURE=6
 		//415,L,distinct_cultivation_boundary
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_LIN.shp NATURE=0
 		//416,L,distinct_vegetation_boundary
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/VEGETATION_LIN.shp NATURE=3 4
 
 		//417,P,prominent_large_tree
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/VEGE/ARB_ISOLE.shp
 		//418,P,prominent_bush_or_tree
 		//419,P,prominent_vegetation_feature
 
-		//501,S,paved_area
+		//501,S,paved_area_with_bn
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/PISTE_AERO.shp
+		//501.1,S,paved_area
 		//502,L,wide_road
 		//503,L,road
 		//504,L,vehicle_track
@@ -113,29 +144,43 @@ public class OriSHP {
 		//513,L,wall
 		//514,L,ruined_wall
 		//515,L,impassable_wall
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_LINE.shp NATURE=4 5
 		//516,L,fence
 		//517,L,ruined_fence
 		//518,L,impassable_fence
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_LINE.shp NATURE=6
 		//519,PO,crossing_point
 		//520,S,area_shall_not_entered
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CIMETIERE.shp
 		//521,S,building
 		clipSHP(inBasePath + "BATI/BATIMENT.shp", outBasePath+"521_building.shp", envClip);
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_SURF.shp
+		//521.1,P,building_min
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_PONCT.shp NATURE= 9 10 11 13
+		//521.4,L,building_outline
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/LIM_TOIT.shp
 		//522,S,canopy
-		//523,S,ruin
+		//523,L,ruin
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_LINE.shp NATURE=3
 		//524,P,high_tower
 		clipSHP(inBasePath + "VFTE/PYLONE.shp", outBasePath+"524_high_tower.shp", envClip);
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_PONCT.shp NATURE=1
 		//525,P,small_tower
 		//526,P,cairn
 		clipSHP(inBasePath + "GEO/POINT_GEOD.shp", outBasePath+"526_cairn.shp", envClip);
-		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ADM/BORN_FRONT.shp
-		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ADM/POINT_FRON.shp
+		///add home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ADM/BORN_FRONT.shp
+		///add home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ADM/POINT_FRON.shp
 		//527,P,fodder_rack
 		//528,L,prominent_line_feature
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_LINE.shp NATURE=1 2
 		//529,L,prominent_impassable_line_feature
 		//530,P,prominent_man_made_feature_ring
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/BATI/CONSTRUC_PONCT.shp NATURE= 2 to 8
 		//531,P,prominent_man_made_feature_x
 
 		//601,L,magnetic_north_line
+		//603.0,P,spot_height
+		///home/juju/Bureau/orienteering/omap_luxembourg_shp/BDLTC_SHP/ALTI/POINT_COTE.shp ECHELLE=1
 
 		System.out.println("end");
 	}
