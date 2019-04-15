7e4a538eb13eed7dbd16f948c98fb02dd1c622bf
jgaffuri <jgaf@protonmail.com>
2019-04-15 17:01:04 +0200

    a

--- src/main/java/org/orimap/OriLuxSHP.java
+++ src/main/java/org/orimap/OriLuxSHP.java
@@ -25,6 +25,8 @@ public class OriLuxSHP {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Start");
 
+		//TODO get /home/juju/Bureau/orienteering/omap_luxembourg_shp/shp/ into ori comp
+
 		String basePathBDT = "/home/juju/Bureau/orienteering/data/BDLTC_SHP/";
 		String basePathOSM = "/home/juju/Bureau/orienteering/data/OSM/luxembourg-latest-free.shp/";
 		String basePathCadastre = "/home/juju/Bureau/orienteering/data/pcn-cadastre/";
@@ -34,10 +36,10 @@ public class OriLuxSHP {
 		String basePathOriCadastre = "/home/juju/Bureau/orienteering/data/ori_cadastre/";
 
 
-		System.out.println("BDT to ori");
-		extractBDTToOri(basePathBDT, basePathOriBDT);
-		System.out.println("OSM to ori");
-		extractOSMToOri(basePathOSM, basePathOriOSM);
+		//System.out.println("BDT to ori");
+		//extractBDTToOri(basePathBDT, basePathOriBDT);
+		//System.out.println("OSM to ori");
+		//extractOSMToOri(basePathOSM, basePathOriOSM);
 		System.out.println("cadastre to ori");
 		extractCadastreToOri(basePathCadastre, basePathOriCadastre);
 
@@ -215,6 +217,7 @@ public class OriLuxSHP {
 	}
 
 	public static void extractOSMToOri(String inBasePath, String outBasePath) throws Exception {
+		//TODO complete
 
 		//101_L_contour
 		//102_L_index_contour
@@ -312,7 +315,7 @@ public class OriLuxSHP {
 		//518_L_impassable_fence
 		//519,PO,crossing_point
 		//520_S_area_shall_not_entered
-		extractSHP(inBasePath + "gis_osm_landuse_a_free_1_LUXPROJ.shp", outBasePath+"520_S_area_shall_not_entered2.shp", null, CQL.toFilter( "fclass = 'residential'" ));
+		extractSHP(inBasePath + "gis_osm_landuse_a_free_1_LUXPROJ.shp", outBasePath+"520_S_area_shall_not_entered.shp", null, CQL.toFilter( "fclass = 'residential'" ));
 		//521_S_building
 		//521.1_P_building_min
 		//521.4_L_building_outline
@@ -332,7 +335,182 @@ public class OriLuxSHP {
 	}
 
 	public static void extractCadastreToOri(String inBasePath, String outBasePath) throws Exception {
-		//TODO
+		//5048	point géodésique
+
+		//101_L_contour
+		//102_L_index_contour
+		//103_L_form_line
+		//104_L_earth_bank
+		//105_L_earth_wall
+		//106_L_ruined_earth_wall
+		//107_L_erosion_gully
+		//108_L_small_erosion_gully
+		//109_P_small_knoll
+		//110,PO,small_elongated_knoll
+		//111_P_small_depression
+		//112_P_pit
+		//113_S_broken_ground
+		//114_S_very_broken_ground
+		//115_P_prominent_landform_feature
+		//201_L_impassable_cliff
+		//202_L_cliff
+		//203_P_rocky_pit_cave
+		//204_P_boulder
+		//205_P_large_boulder
+		//206_S_gigantic_boulder
+		//5050	rochers
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"206_S_gigantic_boulder.shp", null, CQL.toFilter( "CODE_NATUR = 5050" ) );
+		//207_P_boulder_cluster
+		//208_S_boulder_field
+		//209_S_dense_boulder_field
+		//210_S_stony_ground_slow_running
+		//211_S_stony_ground_walk
+		//212_S_stony_ground_fight
+		//213_S_sandy_ground
+		//214_S_bare_rock
+		//215_L_trench
+
+		//301_S_uncrossable_body_of_water
+		//5000	rivière
+		//5001	ruisseau
+		//5002	canal
+		//5003	étang
+		//5005	bassin
+		//5006	surface hydrographique
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"301_S_uncrossable_body_of_water.shp", null, CQL.toFilter( "CODE_NATUR = 5000 OR CODE_NATUR = 5001 OR CODE_NATUR = 5002 OR CODE_NATUR = 5003 OR CODE_NATUR = 5005 OR CODE_NATUR = 5006" ) );
+		//302_S_shallow_body_of_water
+		//303_P_waterhole
+		//304_L_crossable_watercourse
+		//305_L_small_crossable_watercourse
+		//306_L_minor_seasonal_water_channel
+		//307_S_uncrossable_marsh
+		//308_S_marsh
+		//5004	zone humide
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"308_S_marsh.shp", null, CQL.toFilter( "CODE_NATUR = 5004" ) );
+		//309_L_narrow_marsh
+		//310_S_indistinct_marsh
+		//311_P_well_fountain_water_tank
+		//312_P_spring
+		//313_P_prominent_water_feature
+
+		//401_S_open_land
+		//5008	pâture
+		//5009	pré
+		//5010	vaine
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"401_S_open_land.shp", null, CQL.toFilter( "CODE_NATUR = 5008 OR CODE_NATUR = 5009 OR CODE_NATUR = 5010" ) );
+		//402_S_open_land_scattered_trees
+		//402.1_S_open_land_scattered_trees
+		//403_S_rough_open_land
+		/*5040	sentier
+		5037	chemin repris
+		5038	chemin d'exploitation
+		5027	parc
+		5013	jardin
+		5028	place verte
+		5033	terrain des sports
+		5030	aire de loisirs
+		5049	site archéologique
+		5021	carrière
+		5022	crassier
+		5051	nature multiple*/
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"403_S_rough_open_land.shp", null, CQL.toFilter( "CODE_NATUR = 5040 OR CODE_NATUR = 5037 OR CODE_NATUR = 5038 OR CODE_NATUR = 5027 OR CODE_NATUR = 5013 OR CODE_NATUR = 5028 OR CODE_NATUR = 5033 OR CODE_NATUR = 5030 OR CODE_NATUR = 5049 OR CODE_NATUR = 5021 OR CODE_NATUR = 5022 OR CODE_NATUR = 5051" ) );
+		//404_S_rough_open_land_scattered_trees
+		//405_S_forest
+		//5017	bois
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"405_S_forest.shp", null, CQL.toFilter( "CODE_NATUR = 5017" ) );
+		//406_S_Vegetation_slow
+		//407_S_vegetation_slow_good_visibility
+		//408_S_vegetation_walk
+		/*5052	bande écologique
+		5053	réserve naturelle
+		5019	haie
+		5020	plantation*/
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"408_S_vegetation_walk.shp", null, CQL.toFilter( "CODE_NATUR = 5052 OR CODE_NATUR = 5053 OR CODE_NATUR = 5019 OR CODE_NATUR = 5020" ) );
+		//409_S_vegetation_walk_good_visibility
+		//5018	broussailles
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"409_S_vegetation_walk_good_visibility.shp", null, CQL.toFilter( "CODE_NATUR = 5018" ) );
+		//410_S_vegetation_fight
+		//411_S_vegetation_impassable
+		//411.2_L_vegetation_impassable_minw
+		//412_S_cultivated_land
+		//5007	terre labourable
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"412_S_cultivated_land.shp", null, CQL.toFilter( "CODE_NATUR = 5007" ) );
+		//413_S_orchard
+		//5016	verger
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"413_S_orchard.shp", null, CQL.toFilter( "CODE_NATUR = 5016" ) );
+		//414_S_vineyard_similar
+		//5011	vigne
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"414_S_vineyard_similar.shp", null, CQL.toFilter( "CODE_NATUR = 5011" ) );
+		//415_L_distinct_cultivation_boundary
+		//416_L_distinct_vegetation_boundary
+
+		//417_P_prominent_large_tree
+		//418_P_prominent_bush_or_tree
+		//419_P_prominent_vegetation_feature
+
+		//501_S_paved_area_with_bn
+		//501.1_S_paved_area
+		/*5035	autoroute
+		5036	route nationale
+		5041	rue
+		5039	piste cyclable
+		5024	place
+		5025	place (occupée)
+		5032	piste d'essai
+		5043	place voirie
+		 */
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"501.1_S_paved_area.shp", null, CQL.toFilter( "CODE_NATUR = 5035 OR CODE_NATUR = 5036 OR CODE_NATUR = 5041 OR CODE_NATUR = 5039 OR CODE_NATUR = 5024 OR CODE_NATUR = 5025 OR CODE_NATUR = 5032 OR CODE_NATUR = 5043" ) );
+		//502_L_wide_road
+		//503_L_road
+		//504_L_vehicle_track
+		//505_L_footpath
+		//506_L_small_footpath
+		//507_L_less_distinct_small_footpath
+		//508_L_narrow_ride_or_linear_trace
+
+		//509_L_railway
+
+		//510_L_power_line_cableway_skilift
+		//511_L_major_power_line
+
+		//512_L_bridge_tunnel
+
+		//513_L_wall
+		//514_L_ruined_wall
+		//515_L_impassable_wall
+		//516_L_fence
+		//517_L_ruined_fence
+		//518_L_impassable_fence
+		//519,PO,crossing_point
+		//520_S_area_shall_not_entered
+		/*5023	décharge
+		5026	cimetière
+		5034	camp militaire
+		5031	champ de tir
+		5044	voie ferrée
+		5029	camping
+		5045	aéroport
+		5046	écluse
+		5047	port
+		 */
+		extractSHP(inBasePath + "PARCELLES.shp", outBasePath+"520_S_area_shall_not_entered.shp", null, CQL.toFilter( "CODE_NATUR = 5023 OR CODE_NATUR = 5026 OR CODE_NATUR = 5034 OR CODE_NATUR = 5031 OR CODE_NATUR = 5044 OR CODE_NATUR = 5029 OR CODE_NATUR = 5045 OR CODE_NATUR = 5046 OR CODE_NATUR = 5047" ) );
+		//521_S_building
+		extractSHP(inBasePath + "BATIMENTS.shp", outBasePath+"521_S_building.shp");
+		//521.1_P_building_min
+		//521.4_L_building_outline
+		//522_S_canopy
+		//523_L_ruin
+		//524_P_high_tower
+		//525_P_small_tower
+		//526_P_cairn
+		//527_P_fodder_rack
+		//528_L_prominent_line_feature
+		//529_L_prominent_impassable_line_feature
+		//530_P_prominent_man_made_feature_o
+		//531_P_prominent_man_made_feature_x
+
+		//601_L_magnetic_north_line
+		//603.0_P_spot_height
 	}
 
 
@@ -350,6 +528,7 @@ public class OriLuxSHP {
 
 
 	//clip ori data
+	//TODO not tested
 	public void clipOri(String inPath, String outPath, Envelope clipEnv) {
 		for(String file : loadOriSchema()) {
 			if(new File(inPath + file).exists())
@@ -373,27 +552,4 @@ public class OriLuxSHP {
 		return out;
 	}
 
-
-	/*
-		//createSHPRepository("/home/juju/Bureau/orienteering/omap_luxembourg_shp/shpDev/");
-
-
-	//create empty shp files for orienteering map template
-	public static void createSHPRepository(String path) {
-		//ensure folders are there
-		new File(path).mkdirs();
-
-		try {
-			Iterable<CSVRecord> entries = CSVFormat.RFC4180.withHeader("code", "geom", "desc").parse(new FileReader(specsFile));
-			for (CSVRecord e : entries) {
-				System.out.println(e.get("code") + " - " + e.get("geom") + " - " + e.get("desc"));
-				//create SHP
-			}
-		}
-		catch (FileNotFoundException e) { e.printStackTrace(); }
-		catch (IOException e) { e.printStackTrace(); }
-
-
-	}
-	 */
 }
