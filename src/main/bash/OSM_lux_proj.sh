462e0f5ef3761d7dcb4eb8d08c4eae73f9c5f324
jgaffuri <jgaf@protonmail.com>
2019-04-20 10:41:09 +0200

    a

--- src/main/bash/OSM_lux_proj.sh
+++ src/main/bash/OSM_lux_proj.sh
@@ -1,4 +1,9 @@
 
+for layer in AIRE_AMEN ALLEE ARB_ISOLE AXE_CHEMIN AXE_ROUTE AXE_SENTIER BARRAGE
+do
+echo "------ "$layer
 
-ogr2ogr -f "ESRI Shapefile" gis_osm_buildings_a_free_1_LUXPROJ.shp gis_osm_buildings_a_free_1.shp -t_srs EPSG:2169 -s_srs EPSG:4326
+#ogr2ogr -f "ESRI Shapefile" gis_osm_buildings_a_free_1_LUXPROJ.shp gis_osm_buildings_a_free_1.shp -t_srs EPSG:2169 -s_srs EPSG:4326
+#ogr2ogr -overwrite -f "DXF" $outdir$layer".dxf" $outdir$layer".shp"
 
+done
