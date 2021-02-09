fb2ffdc8b01cf3a6a4066f587f3072547135f627
jgaffuri <jgaf@protonmail.com>
2021-02-09 21:11:27 +0100

    a

---
+++ src/main/py/lidardemdownload.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import urllib.request
+
+
+#https://stereo.dev.openstreetmap.org/mappers-delight/dem-classy/
+#https://stereo.dev.openstreetmap.org/mappers-delight/dem/
+
+#https://stereo.dev.openstreetmap.org/mappers-delight/dem/LIDAR2019_NdP_49000_108000_EPSG2169.tif
+#https://stereo.dev.openstreetmap.org/mappers-delight/dem/LIDAR2019_NdP_49000_108000_EPSG2169.tfw
+
+
+
+def download(path, xMin, yMin, xMax, yMax):
+   print("Download")
+
+   baseURL = "https://stereo.dev.openstreetmap.org/mappers-delight/"
+
+   Path(path + "/dem-classy/").mkdir(parents=True, exist_ok=True)
+   Path(path + "/dem/").mkdir(parents=True, exist_ok=True)
+
+   x = 49000
+   y = 108000
+
+   f = "/dem-classy/LIDAR2019_NdP_"+x+"_"+y+"_EPSG2169.tif"
+   if not Path(path+f).exists(): urllib.request.urlretrieve(baseURL+f, path+f)
+
+
+#download("")
