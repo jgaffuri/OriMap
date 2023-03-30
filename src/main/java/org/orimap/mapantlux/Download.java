5e67267b4aeb3425ecd97f394edc10dfeea87b17
jgaffuri <jgaf@proton.me>
2023-03-30 22:51:22 +0200

    a

--- src/main/java/org/orimap/mapantlux/Download.java
+++ src/main/java/org/orimap/mapantlux/Download.java
@@ -1,18 +1,25 @@
 package org.orimap.mapantlux;
 
+import java.util.ArrayList;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
+
 public class Download {
 
 	public static void main(String[] args) {
-		
+
 		String baseURL = "https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200108-130715/lidar2019-ndp-c0-r26-ll48500-95500-epsg2169.zip";
-		
-		//
-		//https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200108-130727/lidar2019-ndp-c0-r27-ll48500-97000-epsg2169.zip
-		//https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200109-064337/lidar2019-ndp-c1-r27-ll50000-97000-epsg2169.zip
+
+		System.out.println("Load tiles");
+		ArrayList<Feature> ziptiles = GeoData.getFeatures("/home/juju/Bureau/orienteering/lidar/lux_project/lidar_urls.gpkg");
+		System.out.println(ziptiles.size());
+
 
 	}
 
-	
+
+
 
 
 
