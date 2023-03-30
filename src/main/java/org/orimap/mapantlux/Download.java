9ad47053a4838cd9fb4591b18953927d4ec134ba
jgaffuri <jgaf@proton.me>
2023-03-30 22:48:17 +0200

    a

---
+++ src/main/java/org/orimap/mapantlux/Download.java
@@ -0,0 +1,19 @@
+package org.orimap.mapantlux;
+
+public class Download {
+
+	public static void main(String[] args) {
+		
+		String baseURL = "https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200108-130715/lidar2019-ndp-c0-r26-ll48500-95500-epsg2169.zip";
+		
+		//
+		//https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200108-130727/lidar2019-ndp-c0-r27-ll48500-97000-epsg2169.zip
+		//https://download.data.public.lu/resources/lidar-2019-releve-3d-du-territoire-luxembourgeois/20200109-064337/lidar2019-ndp-c1-r27-ll50000-97000-epsg2169.zip
+
+	}
+
+	
+
+
+
+}
