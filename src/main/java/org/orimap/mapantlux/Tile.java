bd4bb01c65696d3aaec4fb8a98b028b5d94664bc
jgaffuri <jgaf@proton.me>
2023-04-01 17:39:38 +0200

    a

---
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.orimap.mapantlux;
+
+/**
+ * @author juju
+ *
+ */
+public class Tile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//see mbtiles
+
+
+	}
+
+}
