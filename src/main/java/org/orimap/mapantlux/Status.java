91108af2db1636c7dab77255136960dfe7eccb5e
jgaffuri <jgaf@proton.me>
2023-04-02 16:28:46 +0200

    a

--- src/main/java/org/orimap/mapantlux/Status.java
+++ src/main/java/org/orimap/mapantlux/Status.java
@@ -4,47 +4,110 @@
 package org.orimap.mapantlux;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.io.geo.CRSUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
 
 /**
  * @author juju
- *
  */
 public class Status {
 	final static Logger LOGGER = LogManager.getLogger(Status.class.getName());
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	private final static String path = "/home/juju/Bureau/orienteering/lidar/";
+	private final static String pathIn = path + "in/lux/";
+	private final static String pathOut = path + "out/lux/";
+	private final static CoordinateReferenceSystem crs = CRSUtil.getCRS(2169);
+
+	public static void main(String[] args) throws Throwable {
 		LOGGER.info("Start");
 
-		String path = "/home/juju/Bureau/orienteering/lidar/";
-		String pathIn = path + "in/lux/";
-		String pathOut = path + "out/lux/";
-
-		Set<String> files = getFiles(pathIn);
-		for(String f : files) {
-			//System.out.println(f);
-			//LIDAR2019_NdP_59500_115000_EPSG2169.laz
-			String a = f.replace("LIDAR2019_NdP_", "").replace("_EPSG2169.laz", "");
-			String[] sp = a.split("_");
-			System.out.println(a);
-			System.out.println(sp[0]);
-			System.out.println(sp[1]);
-		}
-
-
-
-
+		//in laz
+		//in();
+		//out png
+		out();
 
 		LOGGER.info("End");
 	}
+
+
+	private static void in() {
+		LOGGER.info("Get input files");
+		Set<String> files = getFiles(pathIn);
+		LOGGER.info(files.size());
+
+		LOGGER.info("Make input files geo");
+		Collection<Feature> fs = new ArrayList<>();
+		for(String f : files) {
+			//System.out.println(f);
+			//LIDAR2019_NdP_59500_115000_EPSG2169.laz
+			String[] sp = f.split("_");
+			int x = Integer.parseInt(sp[2]);
+			int y = Integer.parseInt(sp[3]);
+			Feature ft = new Feature();
+			ft.setAttribute("file", f);
+			ft.setAttribute("x", x);
+			ft.setAttribute("y", y);
+			ft.setGeometry(new GeometryFactory().createPolygon(new Coordinate[] { new Coordinate(x,y), new Coordinate(x+500,y), new Coordinate(x+500,y+500), new Coordinate(x,y+500), new Coordinate(x,y) } ));
+			fs.add(ft);
+		}
+		LOGGER.info(fs.size());
+
+		LOGGER.info("Save");
+		GeoData.save(fs, path+"lux_project/files_input.gpkg", crs);
+	}
+
+
+
+	private static void out() throws Throwable {
+		LOGGER.info("Get output files");
+		Set<String> files = getFiles(pathOut);
+		LOGGER.info(files.size());
+
+		LOGGER.info("Make output files geo");
+		Collection<Feature> fs = new ArrayList<>();
+		for(String f : files) {
+			if(f.contains("_EPSG2169.laz_depr.png")) continue;
+			if(f.contains("_EPSG2169.laz_depr.pgw")) continue;
+			if(f.contains("_EPSG2169.laz.pgw")) continue;
+
+			System.out.println(f);
+			//LIDAR2019_NdP_53000_111000_EPSG2169.laz.png
+			String[] sp = f.split("_");
+			int x = Integer.parseInt(sp[2]);
+			int y = Integer.parseInt(sp[3]);
+			Feature ft = new Feature();
+			ft.setAttribute("file", f);
+			ft.setAttribute("x", x);
+			ft.setAttribute("y", y);
+			ft.setGeometry(new GeometryFactory().createPolygon(new Coordinate[] { new Coordinate(x,y), new Coordinate(x+500,y), new Coordinate(x+500,y+500), new Coordinate(x,y+500), new Coordinate(x,y) } ));
+			fs.add(ft);
+
+			//long size = Files.size(Paths.get(pathOut+f));
+
+
+		}
+		LOGGER.info(fs.size());
+
+		LOGGER.info("Save");
+		GeoData.save(fs, path+"lux_project/files_output.gpkg", crs);
+	}
+
+
+
+
 
 	public static Set<String> getFiles(String dir) {
 		return Stream.of(new File(dir).listFiles())
