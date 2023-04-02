dde84beebcb4fd99f959eab913bb6395f0e6bd44
jgaffuri <jgaf@proton.me>
2023-04-02 23:10:53 +0200

    a

--- src/main/java/org/orimap/mapantlux/Tile.java
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -17,6 +17,9 @@ public class Tile {
 https://gdal.org/programs/gdal2tiles.html
 https://gdal.org/programs/gdal2tiles.html#cmdoption-gdal2tiles-p
 
+Starting with GDAL 3.2, additional profiles are available from tms_XXXX.json files placed in GDAL data directory
+/usr/share/gdal/
+
 gdal2tiles.py --xyz -x -s EPSG:3035 --zoom=0-8 --processes=2 /home/juju/Bureau/gisco/elevation/EU_DEM_mosaic_1000K/1000_temp.vrt /home/juju/Bureau/aaa/
 
 //https://github.com/jgaffuri/mbxyz/blob/main/tms_EUR.json
