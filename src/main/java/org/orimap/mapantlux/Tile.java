9e5dc94e312ce9acec2d832b64036389e082b2aa
jgaffuri <jgaf@proton.me>
2023-04-02 23:22:54 +0200

    a

--- src/main/java/org/orimap/mapantlux/Tile.java
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -20,11 +20,8 @@ https://gdal.org/programs/gdal2tiles.html#cmdoption-gdal2tiles-p
 Starting with GDAL 3.2, additional profiles are available from tms_XXXX.json files placed in GDAL data directory
 /usr/share/gdal/
 
-gdal2tiles.py --xyz -x -s EPSG:3035 --zoom=0-8 --processes=2 /home/juju/Bureau/gisco/elevation/EU_DEM_mosaic_1000K/1000_temp.vrt /home/juju/Bureau/aaa/
+gdal2tiles.py -p lux --xyz --zoom=5-10 -x -s EPSG:2169 --processes=3 /home/juju/Bureau/orienteering/BD_ACT/Shape_50/50_ESTOMPAGE_LUX.tif /home/juju/Bureau/aaa/
 
-//https://github.com/jgaffuri/mbxyz/blob/main/tms_EUR.json
-gdal2tiles.py -p EUR --xyz --zoom=0-5 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
-gdal2tiles.py -p EUR --xyz --zoom=0-8 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
 
 level 7 resolution:
 scale 1:800 000
