b7c3daed488add6b1433b8e612ae844039d00877
jgaffuri <jgaf@proton.me>
2023-04-02 22:57:04 +0200

    a

--- src/main/java/org/orimap/mapantlux/Tile.java
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -12,6 +12,24 @@ public class Tile {
 	public static void main(String[] args) {
 
 		//see mbtiles
+		
+		/*
+https://gdal.org/programs/gdal2tiles.html
+https://gdal.org/programs/gdal2tiles.html#cmdoption-gdal2tiles-p
+
+gdal2tiles.py --xyz -x -s EPSG:3035 --zoom=0-8 --processes=2 /home/juju/Bureau/gisco/elevation/EU_DEM_mosaic_1000K/1000_temp.vrt /home/juju/Bureau/aaa/
+
+gdal2tiles.py -p EUR --xyz --zoom=0-5 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
+gdal2tiles.py -p EUR --xyz --zoom=0-8 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
+
+level 7 resolution:
+scale 1:800 000
+211.6673615 m/pix
+211.6735723 m/pix
+-> 211.7 m/pix
+
+		*/
+		
 		//maybe merge first ? into 6 files ?
 
 
