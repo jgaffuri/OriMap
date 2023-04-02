d704649390df5718bb6d64269e6bdfd70ba95598
jgaffuri <jgaf@proton.me>
2023-04-02 15:53:14 +0200

    a

---
+++ src/main/java/org/orimap/mapantlux/Status.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.orimap.mapantlux;
+
+/**
+ * @author juju
+ *
+ */
+public class Status {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
