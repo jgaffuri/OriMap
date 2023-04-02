e6bd54acaa81a53b477b070f1d0e2877075d8af8
jgaffuri <jgaf@proton.me>
2023-04-02 23:05:33 +0200

    a

--- src/main/java/org/orimap/mapantlux/Tile.java
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -19,6 +19,7 @@ https://gdal.org/programs/gdal2tiles.html#cmdoption-gdal2tiles-p
 
 gdal2tiles.py --xyz -x -s EPSG:3035 --zoom=0-8 --processes=2 /home/juju/Bureau/gisco/elevation/EU_DEM_mosaic_1000K/1000_temp.vrt /home/juju/Bureau/aaa/
 
+//https://github.com/jgaffuri/mbxyz/blob/main/tms_EUR.json
 gdal2tiles.py -p EUR --xyz --zoom=0-5 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
 gdal2tiles.py -p EUR --xyz --zoom=0-8 -x -s EPSG:3035 --processes=3 /home/juju/Bureau/gisco/map_background/map.png /home/juju/Bureau/aaa/
 
