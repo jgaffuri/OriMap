05a750ba1fc7a5057b395883c4d64016bb87a2bd
jgaffuri <jgaf@proton.me>
2023-04-02 23:25:03 +0200

    a

--- src/main/java/org/orimap/mapantlux/Tile.java
+++ src/main/java/org/orimap/mapantlux/Tile.java
@@ -20,7 +20,7 @@ https://gdal.org/programs/gdal2tiles.html#cmdoption-gdal2tiles-p
 Starting with GDAL 3.2, additional profiles are available from tms_XXXX.json files placed in GDAL data directory
 /usr/share/gdal/
 
-gdal2tiles.py -p lux --xyz --zoom=5-10 -x -s EPSG:2169 --processes=3 /home/juju/Bureau/orienteering/BD_ACT/Shape_50/50_ESTOMPAGE_LUX.tif /home/juju/Bureau/aaa/
+gdal2tiles.py -p lux --xyz --zoom=6-15 -x -s EPSG:2169 --processes=3 /home/juju/Bureau/orienteering/BD_ACT/Shape_50/50_ESTOMPAGE_LUX.tif /home/juju/Bureau/aaa/
 
 
 level 7 resolution:
