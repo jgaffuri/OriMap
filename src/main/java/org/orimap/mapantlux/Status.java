2157839f2d2acafa65eecdfb8b43e7a7550f8959
jgaffuri <jgaf@proton.me>
2023-04-02 15:58:20 +0200

    a

--- src/main/java/org/orimap/mapantlux/Status.java
+++ src/main/java/org/orimap/mapantlux/Status.java
@@ -3,18 +3,44 @@
  */
 package org.orimap.mapantlux;
 
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author juju
  *
  */
 public class Status {
+	final static Logger LOGGER = LogManager.getLogger(Status.class.getName());
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LOGGER.info("Start");
 
+		String path = "/home/juju/Bureau/orienteering/lidar/";
+		String pathIn = path + "in/lux/";
+		String pathOut = path + "out/lux/";
+
+
+
+
+
+
+		LOGGER.info("End");
+	}
+
+	public static Set<String> listFilesUsingJavaIO(String dir) {
+		return Stream.of(new File(dir).listFiles())
+				.filter(file -> !file.isDirectory())
+				.map(File::getName)
+				.collect(Collectors.toSet());
 	}
 
 }
