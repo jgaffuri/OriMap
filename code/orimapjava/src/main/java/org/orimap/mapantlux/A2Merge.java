df9990252968cc035e26013f67778ae437f66313
jgaffuri <jgaf@proton.me>
2023-04-06 10:58:04 +0200

    a

--- code/orimapjava/src/main/java/org/orimap/mapantlux/A2Merge.java
+++ code/orimapjava/src/main/java/org/orimap/mapantlux/A2Merge.java
@@ -77,11 +77,11 @@ public class A2Merge {
 
 					//System.out.println(f);
 
-					//LOGGER.info("   to RGB");
-					//String cmd = "pct2rgb.py " + f + " " + f;
+					LOGGER.info("   to RGB");
+					String cmd = "pct2rgb.py " + f + " " + f;
 					//System.out.println(cmd);
-					//A3Merge.run(cmd, false);
-					
+					A2Merge.run(cmd, false);
+
 					sb.append(f);
 					sb.append(" ");
 
@@ -95,49 +95,9 @@ public class A2Merge {
 				//gdal_merge.py -o /home/juju/Bureau/orienteering/lidar/out/lux.tiff /home/juju/Bureau/orienteering/lidar/out/lux/LIDAR2019_NdP_54500_112500_EPSG2169.laz.png /home/juju/Bureau/orienteering/lidar/out/lux/LIDAR2019_NdP_54500_112000_EPSG2169.laz.png
 				//LOGGER.info("   " + cmd);
 				A2Merge.run(cmd, true);
-				
+
 				LOGGER.info("   Build pyramids with gdaladdo");
 				A2Merge.run("gdaladdo -r average "+pathOut+"lux_merged/lux"+sign+".tiff", true);
-
-				
-				/*
-				LOGGER.info("   Make output files list");
-				int nb=0;
-				BufferedWriter writer = new BufferedWriter(new FileWriter(listFile));
-
-				for(String f : files) {
-					if(f.contains("_EPSG2169.laz_depr.png")) continue;
-					if(f.contains("_EPSG2169.laz_depr.pgw")) continue;
-					if(f.contains("_EPSG2169.laz.pgw")) continue;
-					if(f.contains("_undergrowth")) continue;
-					if(f.contains("_vege")) continue;
-					if(f.contains(".xml")) continue;
-					if(f.contains(".pgw")) continue;
-
-					//exclude files out of the tile
-					String f2 = f.replace(pathOut+"lux/", "");
-					String[] sp = f2.split("_");
-					int x_ = Integer.parseInt(sp[2]);
-					if(x_<x) continue;
-					if(x_>=x+step) continue;
-					int y_ = Integer.parseInt(sp[3]);
-					if(y_<y) continue;
-					if(y_>=y+step) continue;
-
-					//System.out.println(f);
-					writer.write(f);
-					writer.write("\n");
-
-					nb++;
-				}
-				writer.close();
-				LOGGER.info("   "+nb);
-				if(nb == 0) continue;
-
-				LOGGER.info("   Run gdalbuildvrt");
-				String cmd = "gdalbuildvrt -input_file_list " +listFile+ " -overwrite " +pathOut+"lux_merged/lux"+sign+".vrt";
-				LOGGER.info("   " + cmd);
-				A3Merge.run(cmd, true);*/
 
 			}
 
@@ -178,5 +138,5 @@ public class A2Merge {
 		}
 	}
 
-	
+
 }
