e271751d4e90d3a2c711ea7aecad2000d16edbe2
jgaffuri <jgaf@proton.me>
2023-04-07 09:22:24 +0200

    a

--- code/orimapjava/src/main/java/org/orimap/mapantlux/A2Merge.java
+++ code/orimapjava/src/main/java/org/orimap/mapantlux/A2Merge.java
@@ -3,7 +3,13 @@ package org.orimap.mapantlux;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Set;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -34,6 +40,11 @@ public class A2Merge {
 
 	public static void main(String[] args) throws Throwable {
 		LOGGER.info("Start");
+
+
+		isIndexed(pathOut+"out/lux/LIDAR2019_NdP_59000_97000_EPSG2169.laz.png");
+		//isIndexed(pathOut+"out/lux/LIDAR2019_NdP_96500_75500_EPSG2169.laz.png");
+		System.exit(0);
 
 		new File(pathOut + "lux_merged/").mkdirs();
 
@@ -77,11 +88,12 @@ public class A2Merge {
 
 					//System.out.println(f);
 
-					//TODO do this only when image is rgb
-					//LOGGER.info("   to RGB");
-					//String cmd = "pct2rgb.py " + f + " " + f;
-					//System.out.println(cmd);
-					//A2Merge.run(cmd, false);
+					if(isIndexed(f)) {
+						//LOGGER.info("   to RGB");
+						String cmd = "pct2rgb.py " + f + " " + f;
+						//System.out.println(cmd);
+						A2Merge.run(cmd, false);
+					}
 
 					sb.append(f);
 					sb.append(" ");
@@ -105,6 +117,103 @@ public class A2Merge {
 
 		LOGGER.info("End");
 	}
+
+
+
+
+	/**
+	 * Check if an image file is indexed (not in RGB)
+	 * 
+	 * @param f
+	 * @return
+	 * @throws Throwable 
+	 */
+	private static boolean isIndexed(String f) throws Throwable {
+
+		//TODO
+		//use gdalinfo ?
+		//or https://stackoverflow.com/questions/16115851/read-image-metadata-from-single-file-with-java
+
+
+
+		File file = new File( f );
+		ImageInputStream iis = ImageIO.createImageInputStream(file);
+		Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+		if (readers.hasNext()) {
+			// pick the first available ImageReader
+			ImageReader reader = readers.next();
+			// attach source to the reader
+			reader.setInput(iis, true);
+			// read metadata of first image
+			IIOMetadata metadata = reader.getImageMetadata(0);
+			String[] names = metadata.getMetadataFormatNames();
+			int length = names.length;
+			for (int i = 0; i < length; i++) {
+				System.out.println( "Format name: " + names[ i ] );
+				System.out.println( metadata.getAsTree(names[i]) );
+				//displayMetadata(metadata.getAsTree(names[i]));
+			}
+		}
+
+
+		/*
+
+gdalinfo LIDAR2019_NdP_59000_97000_EPSG2169.laz.png 
+
+Driver: PNG/Portable Network Graphics
+Files: LIDAR2019_NdP_59000_97000_EPSG2169.laz.png
+       LIDAR2019_NdP_59000_97000_EPSG2169.laz.pgw
+Size is 1183, 1183
+Origin = (58999.930000000029395,96999.999999999970896)
+Pixel Size = (0.423333333333333,-0.423333333333333)
+Corner Coordinates:
+Upper Left  (   58999.930,   97000.000) 
+Lower Left  (   58999.930,   96499.197) 
+Upper Right (   59500.733,   97000.000) 
+Lower Right (   59500.733,   96499.197) 
+Center      (   59250.332,   96749.598) 
+Band 1 Block=1183x1 Type=Byte, ColorInterp=Palette
+  Image Structure Metadata:
+    NBITS=4
+  Color Table (RGB with 9 entries)
+    0: 255,255,255,255
+    1: 166,85,43,255
+    2: 180,246,180,255
+    3: 160,239,160,255
+    4: 140,231,140,255
+    5: 200,254,200,255
+    6: 255,219,166,255
+    7: 0,0,0,255
+    8: 120,224,120,255
+
+gdalinfo LIDAR2019_NdP_96500_75500_EPSG2169.laz.png
+
+Driver: PNG/Portable Network Graphics
+Files: LIDAR2019_NdP_96500_75500_EPSG2169.laz.png
+       LIDAR2019_NdP_96500_75500_EPSG2169.laz.png.aux.xml
+Size is 1183, 1184
+Origin = (96499.980000000025029,75499.989999999976135)
+Pixel Size = (0.423333333333333,-0.423333333333333)
+Image Structure Metadata:
+  INTERLEAVE=PIXEL
+Corner Coordinates:
+Upper Left  (   96499.980,   75499.990) 
+Lower Left  (   96499.980,   74998.763) 
+Upper Right (   97000.783,   75499.990) 
+Lower Right (   97000.783,   74998.763) 
+Center      (   96750.382,   75249.377) 
+Band 1 Block=1183x1 Type=Byte, ColorInterp=Red
+Band 2 Block=1183x1 Type=Byte, ColorInterp=Green
+Band 3 Block=1183x1 Type=Byte, ColorInterp=Blue
+
+		 */	
+
+		return false;
+	}
+
+
+
+
 
 
 
