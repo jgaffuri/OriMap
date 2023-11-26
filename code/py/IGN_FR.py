51f4bcc0498e2ad59de146aa336a9593ac5f4e04
jgaffuri <jgaf@proton.me>
2023-11-26 20:29:03 +0100

    a

--- code/py/IGN_FR.py
+++ code/py/IGN_FR.py
@@ -1,8 +1,10 @@
 
-print("Hi !")
+# https://geoservices.ign.fr/documentation/services/api-et-services-ogc/images-wms-ogc
+# https://geoservices.ign.fr/documentation/services/api-et-services-ogc/images-tuilees-wmts-ogc
 
-
+# read key
 with open('/home/juju/clef_ign.txt', 'r') as f:
     clef = f.read()
 
 print(clef)
+
