e6c8f5087cd54765614d4393b4e050624655a724
jgaffuri <jgaf@proton.me>
2024-11-10 12:46:33 +0100

    a

---
+++ code/py/download_dalles.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+def download_files(file_path, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
+
+    with open(file_path, 'r') as f: urls = f.readlines()
+
+    for url in urls:
+        url = url.strip()
+        if not url: continue
+
+        try:
+            file_name = os.path.basename(url)
+            output_path = os.path.join(output_folder, file_name)
+            response = requests.get(url, stream=True)
+            response.raise_for_status()  # Raise an error for failed requests
+            with open(output_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+
+            print(f"Downloaded {file_name} to {output_folder}")
+        
+        except requests.RequestException as e:
+            print(f"Failed to download {url}: {e}")
+
+
+download_files("/home/juju/geodata/lidar/test_1/liste_dalle.txt", "/home/juju/geodata/lidar/test_1/")
