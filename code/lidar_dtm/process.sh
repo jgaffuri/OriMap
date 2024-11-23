5785518022cba9de400779cda3c5909f81b89aad
jgaffuri <jgaf@proton.me>
2024-11-23 14:57:56 +0100

    a

--- code/lidar_dtm/process.sh
+++ code/lidar_dtm/process.sh
@@ -4,7 +4,17 @@ echo "pipeline"
 pdal pipeline p.json
 
 echo "hillshading" 
-gdaldem hillshade "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/hillshade.tif" -z 1 -s 1 -az 315 -alt 45
+gdaldem hillshade "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/hillshade.tiff" -z 1 -s 1 -az 315 -alt 45
 
 echo "slope"
-gdaldem slope "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/slope.tif" -s 1
+gdaldem slope "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/slope.tiff" -s 1
+
+echo "aspect"
+gdaldem aspect "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/aspect.tiff"
+#-zero_for_flat
+
+
+gdal_translate -of PNG "${FOLDER_PATH}pdal_out/hillshade.tif" "${FOLDER_PATH}pdal_out/hillshade.png"
+gdal_translate -of PNG "${FOLDER_PATH}pdal_out/slope.tif" "${FOLDER_PATH}pdal_out/slope.png"
+gdal_translate -of PNG "${FOLDER_PATH}pdal_out/aspect.tif" "${FOLDER_PATH}pdal_out/aspect.png"
+
