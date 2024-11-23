61c6c772ebb1e46601fff530374457dbe693097f
jgaffuri <jgaf@proton.me>
2024-11-23 14:34:27 +0100

    a

--- code/lidar_dtm/process.sh
+++ code/lidar_dtm/process.sh
@@ -1,10 +1,10 @@
 FOLDER_PATH="/home/juju/Bureau/lidar_test/"
 
 echo "pipeline"
-pdal pipeline p4.json
+pdal pipeline p.json
 
 echo "hillshading" 
-gdaldem hillshade "${FOLDER_PATH}pdal_out/dtm_4.tif" "${FOLDER_PATH}pdal_out/hillshade_4.tif" -z 1 -s 1 -az 315 -alt 45
+gdaldem hillshade "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/hillshade.tif" -z 1 -s 1 -az 315 -alt 45
 
 echo "slope"
-gdaldem slope "${FOLDER_PATH}pdal_out/dtm_4.tif" "${FOLDER_PATH}pdal_out/slope_4.tif" -s 1
+gdaldem slope "${FOLDER_PATH}pdal_out/dtm.tif" "${FOLDER_PATH}pdal_out/slope.tif" -s 1
